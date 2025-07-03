30e86e1ac982348d0bc1bbed84bd46738d2dc8e1
Axel Liuzzi <axelliuzzii+coding@gmail.com>
2025-07-03 20:21:19 +0200

    fixed bug

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,38 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import bcrypt from 'bcrypt'
-import { User } from "@/types/types";
-
+import bcrypt from "bcrypt";
+import { AppUser } from "@/types/types";
 
 export async function POST(request: NextRequest) {
-    try{
-        const body: User = await request.json();
-        const hashedpassword = await bcrypt.hash(body.password!, 10);
-        
+  try {
+    const body: AppUser = await request.json();
+    const hashedpassword = await bcrypt.hash(body.password!, 10);
 
-        const user = await prisma.users.create({
-            data: {
-                name: body.name,
-                surname: body.surname,
-                nickname: body.nickname,
-                password: hashedpassword,
-                email: body.email,
-                telephone: body.telephone,
-                role_id: body.role_id || 1, // Default to role_id 1 if not provided
-            }
-        });
+    const user = await prisma.users.create({
+      data: {
+        name: body.name,
+        surname: body.surname,
+        nickname: body.nickname,
+        password: hashedpassword,
+        email: body.email,
+        telephone: body.telephone,
+        role_id: body.role_id || 1, // Default to role_id 1 if not provided
+      },
+    });
 
-        return NextResponse.json({ message: "User created successfully" }, { status: 201 });
-    } catch (error) {
+    return NextResponse.json(
+      { message: "User created successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    // errore generico
+    console.error("Error creating user:", error);
 
-        // errore generico
-        console.error("Error creating user:", error);
-
-        //errore mail gia esistente
-        if (error instanceof Error && error.message.includes("Unique constraint failed on the fields: (`email`)")) {
-            return NextResponse.json({ message: "Email already exists" }, { status: 409 });
-        } else {
-            return NextResponse.json({ message: "An error occurred while creating the user" }, { status: 500 });
-        }
+    //errore mail gia esistente
+    if (
+      error instanceof Error &&
+      error.message.includes(
+        "Unique constraint failed on the fields: (`email`)"
+      )
+    ) {
+      return NextResponse.json(
+        { message: "Email already exists" },
+        { status: 409 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "An error occurred while creating the user" },
+        { status: 500 }
+      );
     }
+  }
 }
