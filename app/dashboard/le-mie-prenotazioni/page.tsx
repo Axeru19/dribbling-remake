5337005797fdf1677aa6f3ed0803504742cf9810
Axel Liuzzi <axelliuzzii+coding@gmail.com>
2026-01-21 17:35:47 +0100

    fixed bug

--- app/dashboard/le-mie-prenotazioni/page.tsx
+++ app/dashboard/le-mie-prenotazioni/page.tsx
@@ -1,5 +1,35 @@
+import { authOptions } from "@/lib/auth";
+import { ReservationStatus } from "@/lib/enums";
+import { prisma } from "@/lib/prisma";
+import { AppUser } from "@/types/types";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { toast } from "sonner";
 
-export default function page() {
-  return <div>page</div>;
+export default async function page() {
+  const session = await getServerSession(authOptions);
+  const user: AppUser | null = session?.user || null;
+
+  const reservations = await prisma.reservations.findMany({
+    where: {
+      id_user: BigInt(user?.id!),
+    },
+    orderBy: {
+      date: "asc",
+    },
+  });
+  return (
+    <div>
+      {reservations.map((reservation) => (
+        <div
+          key={reservation.id.toString()}
+          className="p-4 mb-4 border rounded"
+        >
+          <h3 className="text-lg font-semibold">
+            Reservation ID: {reservation.id.toString()}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
 }
